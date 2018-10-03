e0d09b96b698833cf2232e816e2784070df8f02c
Enes KURU <enes793k@icloud.com>
2018-10-03 14:21:08 +0300

    async action with thunk/axios example

--- src/actions/user-actions.js
+++ src/actions/user-actions.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const UPDATE_USER = 'USER_UPDATE';
-export const GET_USERS = 'GET_USERS';
+export const GET_USER_ERROR = 'GET_USER_ERROR';
 
 export function updateUser(newUser) {
   return {
@@ -12,10 +12,29 @@ export function updateUser(newUser) {
   }
 }
 
+/*
 export function getUsers() {
   return dispatch => {
     axios.get('https://jsonplaceholder.typicode.com/users').then(response => response.data)
-      .then(data => console.log(data))
+      .then(userList => console.log(userList))
       .catch(error => console.log(error))
+  }
+}
+*/
+
+export function showError(error) {
+  return {
+    type: GET_USER_ERROR,
+    payload: {
+      error: error.toString()
+    }
+  }
+}
+
+export function getUsers() {
+  return dispatch => {
+    axios.get('https://jsonplaceholder.typicode.com/users/1').then(response => response.data)
+      .then(user => dispatch(updateUser(user.name)))
+      .catch(error => dispatch(showError(error)))
   }
 }
--- src/reducers/userReducer.js
+++ src/reducers/userReducer.js
@@ -1,9 +1,11 @@
-import {UPDATE_USER} from './../actions/user-actions';
+import {UPDATE_USER, GET_USER_ERROR} from './../actions/user-actions';
 
 export default function userReducer(state = '', {type, payload}) {
   switch (type) {
     case UPDATE_USER:
       return payload.user;
+    case GET_USER_ERROR:
+      return payload.error;
     default:
       return state;
   }
